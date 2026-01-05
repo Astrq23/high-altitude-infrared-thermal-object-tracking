4e0278da7a52726c8c002b83a55007c0b19e1226
Hoang Viet <nguyenhoangviet_t67@hus.edu.vn>
2026-01-05 12:30:27 +0700

    small change

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from src.ui import create_ui
 if __name__ == "__main__":
     demo = create_ui()
     demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
+    print("UI is running at http://localhost:7860")
+    
+#
